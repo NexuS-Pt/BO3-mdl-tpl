129222ae265f3e827a51fdd04d78e0ec4c244068
Carlos Santos <c42759@users.noreply.github.com>
2017-01-27 10:38:50 +0000

    Update install.sql

--- db/install.sql
+++ db/install.sql
@@ -1,9 +1,10 @@
-INSERT INTO `{c2r-prefix}_modules` (`folder`, `sort`) VALUES ("{c2r-mod-folder}", 0);
+INSERT INTO `{c2r-prefix}_modules` (`name`, `folder`, `sort`) VALUES ("{c2r-mod-name}", "{c2r-mod-folder}", 0);
 
 CREATE TABLE IF NOT EXISTS `{c2r-prefix}_example` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `f_1` varchar(255) NOT NULL,
-  `f_2` text NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `folder` varchar(255) NOT NULL,
+  `sort` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `id` (`id`)
+  UNIQUE KEY `folder` (`folder`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
