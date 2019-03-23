99c687910a0408f98cf914a351451080db96c740
Carlos Santos <carlos@one-shift.com>
2019-03-23 23:32:17 +0000

    Bug fix user to c9_user; Now you can use object to set up menu item.

--- actions/install.php
+++ actions/install.php
@@ -1,9 +1,10 @@
 <?php
 
-if (isset($_POST["submitInstall"]) && user::isOwner($authData)) {
+if (isset($_POST["submitInstall"]) && c9_user::isOwner($authData)) {
 	$query = bo3::c2r([
 		'mod-name' => $cfg->mdl->name,
 		'mod-folder' => $cfg->mdl->folder,
+		'mod-code' => $db->real_scape_string(bo3::mdl_load("install/object.json")),
 		'prefix' => $cfg->db->prefix
 	], bo3::mdl_load("db/install.sql"));
 
@@ -12,17 +13,17 @@ if (isset($_POST["submitInstall"]) && user::isOwner($authData)) {
 
 		/*
 			You can use Copy function to leave some files in some places of the backoffice
-			
+
 			copy(
 				"modules/{$cfg->mdl->folder}/install/class.0-example.php",
 				"class/class.0-example.php"
 			);
-			
+
 			Remember, this is the only place where you are permitted to do something like this.
 			Add to uninstall options to remove those files you add above.
 		*/
-		
-		
+
+
 		$mdl = bo3::c2r([
 			'lg-message' => $lang["install"]["success"]
 		], bo3::mdl_load("templates-e/install/message.tpl"));
--- actions/uninstall.php
+++ actions/uninstall.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_POST["submitUninstall"]) && user::isOwner($authData)) {
+if (isset($_POST["submitUninstall"]) && c9_user::isOwner($authData)) {
 	$query = bo3::c2r([
 		'mod-folder' => $cfg->mdl->folder,
 		'prefix' => $cfg->db->prefix
@@ -11,12 +11,12 @@ if (isset($_POST["submitUninstall"]) && user::isOwner($authData)) {
 
 		/*
 			Here you need to send commands to delete those files you copy on install
-			
+
 			unlink("class/class.0-example.php");
-			
+
 			This is mandatory. All copied files need to bee removed during the uninstall.
 		*/
-		
+
 		$mdl = bo3::c2r([
 			'lg-message' => $lang["uninstall"]["success"]
 		], bo3::mdl_load("templates-e/uninstall/message.tpl"));
